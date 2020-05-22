2263ce07c2c0e9bbcd440fa6feb911cc11ffdcef
Ahmed Halat <ahmedhalat@gmail.com>
2020-05-22 17:22:08 -0400

    Updated comments

--- server_manager_startup.sh
+++ server_manager_startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Standard ServerManager
-cd /home/server            
+cd /home/server     
+# Grabbing latestversion of the code from fileuploader bucket
 sudo gsutil -m cp  gs://fileuploader-13/ServerManager.js /home/server
 export GOOGLE_APPLICATION_CREDENTIALS="/home/server/Servers-432f295fec07.json"
 exec node ServerManager.js                
