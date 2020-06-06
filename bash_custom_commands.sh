27f2e9fce6a0b2bdb9b6dc1f07243370c76a5b9a
AhmedHalat <ahmedhalat@gmail.com>
2020-06-06 09:26:26 -0400

    Added process killer

--- bash_custom_commands.sh
+++ bash_custom_commands.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
+# ===== Setup =====
+# 1. Create file and Paste commands here:
+#   ~/.my_custom_commands.sh
+# 2. Give access to this file:
+#   chmod +x .my_custom_commands.sh
+# 3. Edit bashrc in root file and add line:
+#   source ~/.my_custom_commands.sh
+
+
 # forward any port to testing.ahmedhalat.com which is running my reverse proxy tool vm
 # Type in bash terminal "forward 8080" to forward port 8080 to testing.ahmedhalat.com
 # This vm is passworded protected and only enabled on use
 function forward () {
   ssh -R 8888:localhost:$1 root@testing.ahmedhalat.com
+}
+
+
+# kill processes running in local by name
+# $1, name of process to kill
+function processKill(){
+  lsof=`lsof -iTCP -sTCP:LISTEN -P| grep "$1[[:blank:]]" -m 1 `
+  pid=$(echo $lsof | awk '{print $2}')
+  name=$(echo $lsof | awk '{print $9}')
+
+  if [ ! -z "$pid" ]; then
+    printf "\e[32mKilling Process \"$1\" PID:$pid at $name \e[m\n"
+    kill -9 $pid
+  else
+    printf "\e[31mProcess \"$1\" not found running in local\e[m\n"
+  fi
 }
