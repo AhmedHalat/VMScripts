063c42901440068f28c6d900d0e892427bf81414
Ahmed Halat <ahmedhalat@gmail.com>
2021-08-26 21:30:22 -0400

    Update bash_custom_commands.sh

--- bash_custom_commands.sh
+++ bash_custom_commands.sh
@@ -9,8 +9,8 @@
 #   source ~/.my_custom_commands.sh
 
 
-# forward any port to testing.ahmedhalat.com which is running my reverse proxy tool vm
-# Type in bash terminal "forward 8080" to forward port 8080 to testing.ahmedhalat.com
+# forward any port to XXXXXX.com which is running my reverse proxy tool vm
+# Type in bash terminal "forward 8080" to forward port 8080 to XXXXXX.com
 # This vm is passworded protected and only enabled on use
 function forward () {
   ssh -R 8888:localhost:$1 root@XXXXXX.com
